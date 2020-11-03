b2971c2cdc4814566fea55a10cd6c3f1e387067d
Callista Bee <callista.lavender.bee@gmail.com>
2020-11-03 10:57:17 -0800

    add sequence read analysis code

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,20 @@ WORKDIR /tf/
 
 RUN apt-get update && apt-get install -y git wget
 
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+    unzip -q awscliv2.zip && \
+    ./aws/install
+
+ENV HOME=/tf/
+RUN python -c "from tensorflow.keras.applications.vgg16 import VGG16; VGG16()"
+RUN chmod -R go+w /tf/.keras
+
 RUN pip install \
     requests \
     pillow \
     git+https://github.com/uwmisl/cupyck \
     tqdm \
     h5py \
-    tables
+    tables \
+    unireedsolomon
 
-RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-    unzip awscliv2.zip && \
-    ./aws/install
-
-ENV HOME=/tf/
-RUN python -c "from tensorflow.keras.applications.vgg16 import VGG16; VGG16()"
-RUN chmod -R go+w /tf/.keras
--- docker.sh
+++ docker.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-dataset_dir=$1
-if [ ! -d "$dataset_dir" ]; then
-    echo "must provide a valid dataset directory"
-    exit 1
+while getopts ":d:s:" Option
+do
+    case $Option in
+        d ) dataset_dir=$OPTARG;;
+        s ) sequencing_dir=$OPTARG;;
+        * ) echo "invalid option specified"; exit 1;;
+    esac
+done
+
+error=0
+mounts=""
+if [ -d "$dataset_dir" ]; then
+    mounts="$mounts -v $(realpath $dataset_dir):/tf/open_images"
+elif [ -n "$dataset_dir" ]; then
+    echo "dataset directory is invalid!"
+    error=1
+fi
+
+if [ -d "$sequencing_dir" ]; then
+    mounts="$mounts -v $(realpath $sequencing_dir):/tf/sequencing"
+elif [ -n "$sequencing_dir" ]; then
+    echo "sequencing directory is invalid!"
+    error=1
 fi
 
 uid=$(id -u $SUDO_USER)
@@ -11,7 +30,7 @@ uid=$(id -u $SUDO_USER)
 docker run \
     --rm -it -u $uid:$uid -p 8888:8888 --gpus all \
     -v $(pwd):/tf/primo \
-    -v $dataset_dir:/tf/open_images \
+    $mounts \
     -e PYTHONPATH=/tf/primo \
     --name primo \
     primo
