8c3c91915115050aefef118f7028c3ac34b9be69
Lee Organick <leeorg@uw.edu>
2021-06-22 10:50:41 -0700

    migrated over documentation comments

--- primo/models/encoder.py
+++ primo/models/encoder.py
@@ -11,6 +11,7 @@ def entropy_regularizer(strength):
     def encoder_entropy(seq_probs):
         seq_probs.shape.assert_is_compatible_with([None, None, 4])
 
+        # Adding a little epsilon (1e-10) so we never take the log of zero (good catch, callie!)
         ent_by_position = -tf.reduce_sum(
             seq_probs * tf.log(seq_probs + 1e-10),
             axis = 2
@@ -31,19 +32,40 @@ def entropy_regularizer(strength):
 class Encoder:
 
     defaults = {
+        # The reason the default encoder input is a 4096-dimensional vector is
+        # because we're representing our images through an embedding that was learned
+        # by a computer vision model known as VGG [1]. We're borrowing the output of the
+        # 2nd fully-connected layer (i.e. the FC2), which spits out a 4096-by-1 vector.
+        #
+        # If you're very curious about VGG's innerworkings, you can see an example tensorflow
+        # implementation here [2, 3].
+        #
+        # Note for future users: If you ever decide to use a different model VGG16,
+        # you'd probably want to change the input dimension here.
+        #
+        # [1] - https://neurohive.io/en/popular-networks/vgg16/
+        # [2] - https://www.cs.toronto.edu/~frossard/post/vgg16/
+        # [3] - https://github.com/kentsommer/VGG16-Image-Retrieval/blob/master/vgg16_example.py#L237
+        #
         "input_dim": 4096,
+
+        # The feature region of our engineered DNA sequence is 80 nucleotides long.
+        # If you use a shorter or longer DNA sequence for your data, you'll want to change this as well.
         "output_len": 80,
 
+        # Regularization penalty post softmax and helps prevent overfitting.
+        # This value, 1e-2, was experimentally determined.
+        # Since this encoder's output is a softmax, a valid range of regularization strength is between 0 and 1.
         "entropy_reg_strength": 1e-2
     }
 
     def __init__(self, model_path = None, **kwargs):
         for arg, val in _default_sequences.items():
             setattr(self, arg, val)
-        
+
         for arg, val in self.defaults.items():
             setattr(self, arg, val)
-            
+
         for arg, val in kwargs.items():
             setattr(self, arg, val)
 
@@ -54,23 +76,29 @@ class Encoder:
                 layers.Reshape([self.output_len, 4]),
                 layers.Activation('softmax'),
                 layers.Lambda(
+                    # Just using the identity because we don't want to transform the softmaxxed output,
+                    # we just want to make sure we learn an output encoding that's regularized (i.e. not crazy complex/over-fitting)
                     lambda x: x,
+
+                    # In inference mode, this does nothing (just passes identity), but when training, this regularizes
+                    # the activations.
+                    # Using an "entropy" regulator because we passed the output through a softmax.
                     activity_regularizer=entropy_regularizer(
                         self.entropy_reg_strength
                     )
                 )
             ], name='encoder')
-            
+
         else:
             self.model = tf.keras.models.load_model(model_path)
-            
+
     def __call__(self, X):
         return self.model(X)
 
     def trainable(self, flag):
         for layer in self.model.layers:
             layer.trainable = flag
-    
+
     def encode_feature_seqs(self, X):
         onehots = self.model.predict(X)
         return seqtools.onehots_to_seqs(onehots)
